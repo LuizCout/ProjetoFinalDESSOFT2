2fb779aeaffdedc2dabe8e711e7ec0c418c496c8
jptristao2007 <jpaguilartristao@gmail.com>
2026-05-25 22:23:28 -0300

    Iniciando jogo e som

--- jogo.py
+++ jogo.py
@@ -6,3 +6,8 @@ import pygame        # Importa a biblioteca pygame, responsável por criar a jan
 import sys           # Importa o módulo sys, usado para encerrar o programa com sys.exit()
 import random        # Importa o módulo random, usado para gerar números aleatórios (posição de itens, comportamento de barris, etc.)
 import math          # Importa o módulo math, usado para funções matemáticas como seno e cosseno (animações de pulso, fumaça, brilho)
+
+# INICIALIZAÇÃO DO PYGAME E DO SOM
+
+pygame.init()        # Inicializa todos os módulos internos do pygame (gráficos, eventos, fonte, etc.)
+pygame.mixer.init()  # Inicializa especificamente o módulo de áudio do pygame para permitir tocar sons e músicas
