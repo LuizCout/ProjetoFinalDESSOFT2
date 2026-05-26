5ea38c911873c8aee52d14c4733a5149eb4d530a
femastandrea <fellipeam@al.insper.edu.br>
2026-05-25 22:30:16 -0300

    configurações de tela e mundo

--- jogo.py
+++ jogo.py
@@ -14,3 +14,8 @@ pygame.mixer.init()  # Inicializa especificamente o módulo de áudio do pygame 
 pygame.mixer.music.load(r"C:\Users\rezen\Downloads\hard_boss_battle_1_bpm200.ogg")  # Carrega o arquivo de música .ogg do caminho especificado para a memória
 pygame.mixer.music.set_volume(0.3)   # Define o volume da música em 30% (0.0 = mudo, 1.0 = máximo)
 pygame.mixer.music.play(-1)          # Começa a tocar a música; o argumento -1 significa que ela vai repetir em loop infinito
+
+# CONFIGURAÇÕES DE TELA E MUNDO
+
+LARGURA, ALTURA = 900, 800   # Define a largura (900 pixels) e a altura (800 pixels) da janela visível do jogo
+MUNDO_ALTURA = 1400          # Define a altura total do mundo do jogo (maior que a tela), permitindo scroll vertical com câmera
