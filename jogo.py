65bc8a0335e3a168bcd4d65d8c101b4041d7d663
LuizCout <rezendecoutinholuiz@gmail.com>
2026-05-25 22:25:01 -0300

    musica de fundo

--- jogo.py
+++ jogo.py
@@ -11,3 +11,6 @@ import math          # Importa o módulo math, usado para funções matemáticas
 
 pygame.init()        # Inicializa todos os módulos internos do pygame (gráficos, eventos, fonte, etc.)
 pygame.mixer.init()  # Inicializa especificamente o módulo de áudio do pygame para permitir tocar sons e músicas
+pygame.mixer.music.load(r"C:\Users\rezen\Downloads\hard_boss_battle_1_bpm200.ogg")  # Carrega o arquivo de música .ogg do caminho especificado para a memória
+pygame.mixer.music.set_volume(0.3)   # Define o volume da música em 30% (0.0 = mudo, 1.0 = máximo)
+pygame.mixer.music.play(-1)          # Começa a tocar a música; o argumento -1 significa que ela vai repetir em loop infinito
